7ccd7c87853157e7ae95acfaa91731d1a284595b
blacktouff <le.toto.02@gmail.com>
2023-11-30 22:58:01 +0100

    ddd

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using jeumotglisse.objets;
+
+Console.WriteLine("Hello, World!");
+Class1 t = new Class1("lala");
+
+Console.WriteLine(t.toString());
+
+Console.Write("Hey jeune lady comment tu t'intitule ? ");
+string nomdeveloppeur = Console.ReadLine();
+
+
+if (nomdeveloppeur == "noe")
+{
+    Console.WriteLine("Wesh " + nomdeveloppeur + "ca farte ?");
+}
+else if (nomdeveloppeur == "yannis")
+{
+    Console.WriteLine("Wesh " + nomdeveloppeur + "ca roule ?");
+}
+else
+{
+    Console.WriteLine("Casse toi povcon");
+}
---
+++ objets/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace jeumotglisse.objets
+{
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    public class Class1
+    {
+        private string toto;
+        public Class1(string toto)
+        {
+            this.toto = toto;
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+        public string toString()
+        {
+            return toto + "";
+        }
+
+    }
+}
